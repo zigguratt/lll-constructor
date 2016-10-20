28df13fefa33a8d6843eb45cfc2f7b51b61c49b5
Daniel Ellison <daniel@syrinx.net>
2016-10-20 13:09:27 -0400

    Initial commit.

---
+++ constructor.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.0;
+
+contract ConstructorTest {
+    function ConstructorTest(uint256 parm);
+    function getHeartbeat() constant returns(uint256);
+    function getParameters() constant returns(uint256);
+}
