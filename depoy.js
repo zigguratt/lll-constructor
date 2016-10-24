d58dee29c3d2b7d08238aab0c3b0ec173d569995
Daniel Ellison <daniel@syrinx.net>
2016-10-24 08:01:36 -0400

    Simplified further, removing unnecessary recording of code length. Added copious comments.

--- depoy.js
+++ depoy.js
@@ -2,6 +2,6 @@ var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var abi = [{"constant":true,"inputs":[],"name":"getParameter","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"parameter","type":"uint256"}],"type":"constructor"}];
 var ConstructorTest = web3.eth.contract(abi);
-var bin = '602280601e606039604052602060406000396000516000556040516060f360e060020a600035046340f4f7b91415601e5760005460205260206020f35b600256';
+var bin = '6020603a60003960005160005560228060186020396020f360e060020a600035046340f4f7b91415601e5760005460005260206000f35b600256';
 var account = web3.eth.coinbase;
 var c = ConstructorTest.new(0xdeadface, {data: bin, from: account, gas: 1000000});
